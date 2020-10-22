b2d14dcd3e934db4d295fc80d880fc0a89f42704
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-22 13:24:45 +0300

    Update solution_9.py

--- task_9/solution_9.py
+++ task_9/solution_9.py
@@ -1,1 +1,11 @@
 # your code here
+a = 'a' * 100
+count = 1
+count1 = 1
+b = 0
+for i in a:
+    b = b + count
+    c = count1 * count1
+    count = count + 2
+    count1 = count1 + 1
+    print(b == c) #я не совсем поняла, какие данные должны выводиться в задаче, поэтому программа 100 раз выдаст True
