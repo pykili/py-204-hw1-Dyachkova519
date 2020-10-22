854de48cb0b9770eb0a48a492a7a0cc2d69e2c6a
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-22 13:23:03 +0300

    Update solution_8.py

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -1,1 +1,11 @@
 # your code here
+n = int(input())
+n1 = 'a' * n #создаем строку длиной n
+summ = 0 #счетчик суммы чисел
+count = 0 #счетчик количества слагаемых
+for i in n1:
+    num = int(input())
+    summ = summ + num
+    count = count + 1
+    if num == 0: 
+        print(summ / count)
