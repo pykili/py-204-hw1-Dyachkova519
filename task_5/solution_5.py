75b06e4e63817a35be66afc8ed3982deb40c3d31
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-22 13:20:21 +0300

    Update solution_5.py

--- task_5/solution_5.py
+++ task_5/solution_5.py
@@ -1,1 +1,45 @@
 # your code here
+string = input()
+count = 1
+yes = 0
+no = 0
+back = 0
+front = 0
+a = ''
+b = ''
+c = ''
+form = ''
+lemma = ''
+if string != '' and string[0] != '#':
+    for i in string:
+        if i == '\t':
+            c = b
+            b = a
+            a = count
+        count = count + 1
+    count = 1
+    for t in string:
+        if count > c and count < b:
+            form = form + t
+        count = count + 1
+    count = 1
+    for x in string:
+        if count > b and count < a:
+            lemma = lemma + x
+        count = count + 1
+    forlemma = ''
+    for y in form:
+        for z in lemma:
+            if z == y:
+                forlemma = forlemma + z
+    if form != lemma and forlemma == lemma:
+        for w in form:
+            if w in 'eiöü':
+                front = front + 1
+            elif w in 'aıou':
+                back = back + 1
+    if front > 0 and back > 0:
+        no = no + 1
+    else:
+        yes = yes + 1
+    print('yes = ', yes, 'no = ', no)
