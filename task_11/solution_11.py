977daa3b43ef6922e0924c87006c73b02f0f9aff
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-22 13:11:06 +0300

    Update solution_11.py

--- task_11/solution_11.py
+++ task_11/solution_11.py
@@ -1,1 +1,24 @@
 # your code here
+string = input()
+count = 1
+first = ''
+second = ''
+firsecond = ''
+for i in string:
+    if i == ' ':
+        a = count
+    count = count + 1
+count = 1
+for t in string:
+    if count < a:
+        first = first + t
+    elif count > a:
+        second = second + i
+for y in second:
+    for z in first:
+        if z == y:
+            firsecond = firsecond + z
+if firsecond == first:
+    print('Левое слово существует в правом')
+else:
+    print('Левое слово не существует в правом')
