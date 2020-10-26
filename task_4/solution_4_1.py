ce2c3d63315268c8ecbc54596e3187379794b075
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-26 15:30:38 +0300

    Update solution_4_1.py

--- task_4/solution_4_1.py
+++ task_4/solution_4_1.py
@@ -1,26 +1,28 @@
-string = input()
 count = 1
 a = ''
 b = ''
 c = ''
 form = ''
 lemma = ''
-if string != '' and string[0] != '#':
-    for i in string:
-        if i == '\t':
-            c = b
-            b = a
-            a = count
-        count = count + 1
-    count = 1
-    for t in string:
-        if count > c and count < b:
-            form = form + t
-        count = count + 1
-    count = 1
-    for x in string:
-        if count > b and count < a:
-            lemma = lemma + x
-        count = count + 1
-    if form != lemma:
-        print(form, lemma) 
+k = ''
+for i in 'k' * 10:
+    string = input()
+    if string != '' and string[0] != '#':
+        for i in string:
+            if i == '\t':
+                c = b
+                b = a
+                a = count
+            count = count + 1
+        count = 1
+        for t in string:
+            if count > c and count < b:
+                form = form + t
+            count = count + 1
+        count = 1
+        for x in string:
+            if count > b and count < a:
+                lemma = lemma + x
+            count = count + 1
+        if form != lemma:
+            print(form, lemma)  
