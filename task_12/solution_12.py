00e18f95ec2a6b239b042ff6d019d0e02f9f6d65
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-26 16:45:38 +0300

    Update solution_12.py

--- task_12/solution_12.py
+++ task_12/solution_12.py
@@ -1,1 +1,19 @@
 # your code here
+a = input()
+count1 = 0
+count2 = 0
+b = a[0]
+c = a[0]
+occured_twice = False
+for i in a:
+    bi1 = b + i
+    count2 = 0
+    count1 += 1
+    for t in a:
+        bi2 = c + t
+        count2 += 1
+        if bi1 == bi2 and count1 != count2 and count1 != 1 and count2 != 1:
+            occured_twice = True
+        c = t
+    b = i
+print(occured_twice)
