70012e7d4dbd4ff43a3354053299e0bda40a5b35
Dyachkova519 <72070056+Dyachkova519@users.noreply.github.com>
2020-10-26 15:32:57 +0300

    Update solution_11.py

--- task_11/solution_11.py
+++ task_11/solution_11.py
@@ -4,6 +4,9 @@ count = 1
 first = ''
 second = ''
 firsecond = ''
+a = ''
+count1 = 0
+count2 = 0
 for i in string:
     if i == ' ':
         a = count
@@ -13,11 +16,15 @@ for t in string:
     if count < a:
         first = first + t
     elif count > a:
-        second = second + i
+        second = second + t
+    count = count + 1
 for y in second:
+    count2 = count2 + 1
     for z in first:
-        if z == y:
+        count1 = count1 + 1
+        if z == y and count1 == count2:
             firsecond = firsecond + z
+    count1 = 0
 if firsecond == first:
     print('Левое слово существует в правом')
 else:
